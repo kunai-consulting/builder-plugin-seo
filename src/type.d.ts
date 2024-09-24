d525dca62fff4c878d649318b59f956f69661f02
jack shelton <jack.shelton@kunaico.com>
2024-09-24 17:30:16 -0500

    yoast declaration types

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,1 +1,33 @@
-declare module '@builder.io/app-context';
+declare module "@builder.io/app-context";
+
+declare module "yoastseo" {
+  export class Paper {
+    constructor(
+      text: string,
+      options: {
+        keyword: string;
+        title: string;
+        titleWidth: number;
+        description: string;
+        url: string;
+        permalink: string;
+      }
+    );
+  }
+
+  export class AbstractResearcher {
+    constructor(paper: Paper);
+  }
+
+  export class ContentAssessor {
+    constructor(researcher: AbstractResearcher);
+    assess(paper: Paper): void;
+    getResults(): Array<{ text: string }>;
+  }
+
+  export class SEOAssessor {
+    constructor(researcher: AbstractResearcher);
+    assess(paper: Paper): void;
+    getResults(): Array<{ text: string }>;
+  }
+}
---
+++ src/yoast.tsx
@@ -0,0 +1,115 @@
+import { Builder, builder } from "@builder.io/react";
+import { registerCommercePlugin as registerPlugin } from "@builder.io/commerce-plugin-tools";
+import pkg from "../package.json";
+import {
+  Paper,
+  AbstractResearcher,
+  ContentAssessor,
+  SEOAssessor,
+} from "yoastseo";
+import React from "react";
+import { useState, useEffect } from "react";
+
+interface AnalysisResult {
+  results: Array<{ text: string }>;
+}
+
+interface YoastSEOAnalysisProps {
+  value: string;
+  keyword: string;
+  title: string;
+  description: string;
+  url: string;
+}
+
+// function YoastSEOAnalysis(props: YoastSEOAnalysisProps) {
+//   const [seoResults, setSeoResults] = useState<AnalysisResult | null>(null);
+//   const [readabilityResults, setReadabilityResults] =
+//     useState<AnalysisResult | null>(null);
+
+//   const { value, keyword, title, description, url } = props;
+
+//   useEffect(() => {
+//     const analyzeContent = () => {
+//       const paper = new Paper(value, {
+//         keyword,
+//         title,
+//         titleWidth: 600,
+//         description,
+//         url,
+//         permalink: url,
+//       });
+
+//       const researcher = new AbstractResearcher(paper);
+//       const contentAssessor = new ContentAssessor(researcher);
+//       const seoAssessor = new SEOAssessor(researcher);
+
+//       contentAssessor.assess(paper);
+//       seoAssessor.assess(paper);
+
+//       setSeoResults({ results: seoAssessor.getResults() });
+//       setReadabilityResults({ results: contentAssessor.getResults() });
+//     };
+
+//     analyzeContent();
+//   }, [value, keyword, title, description, url]);
+
+//   return (
+//     <div>
+//       <h3>SEO Analysis</h3>
+//       {seoResults && (
+//         <ul>
+//           {seoResults.results.map((result, index) => (
+//             <li key={index}>{result.text}</li>
+//           ))}
+//         </ul>
+//       )}
+//       <h3>Readability Analysis</h3>
+//       {readabilityResults && (
+//         <ul>
+//           {readabilityResults.results.map((result, index) => (
+//             <li key={index}>{result.text}</li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
+
+// first thing that works
+// Builder.register("insertMenu", {
+//   name: "Simple components",
+//   items: [{ name: "NotExist" }],
+// });
+
+// export function registerEditorOnLoad(reactionCallback: (actions: any) => void) {
+//   Builder.register("editor.onLoad", reactionCallback);
+// }
+
+// registerPlugin(
+//   {
+//     name: pkg.name,
+//     id: pkg.name,
+//     settings: [
+//       {
+//         name: "exampleField",
+//         type: "string",
+//         required: true,
+//       },
+//     ],
+//     ctaText: `Save`,
+//     noPreviewTypes: true,
+//   },
+//   async (settings) => {
+//     Builder.registerEditor();
+
+//     Builder.register("editor.onLoad", () => {
+//       Builder.register("editor.editTab", {
+//         name: <div>Name</div>,
+//         component: () => <div>Component call</div>,
+//       });
+//     });
+
+//     return {};
+//   }
+// );
